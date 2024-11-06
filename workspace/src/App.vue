01260cc619bdc562048d41713e6b3812cbf75bcc
laomou <rui.mou@foxmail.com>
2024-11-06 06:06:23 +0000

    init code

---
+++ workspace/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Desktop from './components/Desktop.vue'
+</script>
+
+<template>
+  <main>
+    <Desktop />
+  </main>
+</template>
+
+<style scoped></style>
---
+++ workspace/src/components/Desktop.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="desktop">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Desktop",
+  methods: {
+    openWindow(windowName) {
+      alert(`Opening ${windowName}`);
+    }
+  }
+}
+</script>
+
+<style scoped>
+.desktop {
+  position: relative;
+  width: 100%;
+  height: 100vh;
+  background: url('/src/assets/wallpaper.jpg') no-repeat center center;
+  background-size: cover;
+  overflow: hidden;
+}
+</style>
